6449b5735521878c10d0ef6ef89a22a829914411
Simen Lybekk <simen@lybekk.no>
2014-11-17 15:07:32 +0100

    Prettify the KB plugin configuration

--- perpetualfailure/knowledgebase/__init__.py
+++ perpetualfailure/knowledgebase/__init__.py
@@ -1,5 +1,6 @@
+regex_kb_path = '(/([a-z0-9\-\/]+)?)?'
 def configure(config):
-    config.add_route('knowledgebase.article.view', "/kb{path:(/([a-z0-9\/]+)?)?}")
-    config.add_route('knowledgebase.article.edit', "/admin/kb/edit{path:(/([a-z0-9\/]+)?)?}")
-    config.add_route('knowledgebase.article.create', "/admin/kb/create{path:(/([a-z0-9\/]+)?)?}")
+    config.add_route('knowledgebase.article.view', "/kb{path:%s}" % regex_kb_path)
+    config.add_route('knowledgebase.article.edit', "/admin/kb/edit{path:%s}" % regex_kb_path)
+    config.add_route('knowledgebase.article.create', "/admin/kb/create{path:%s}" % regex_kb_path)
 
