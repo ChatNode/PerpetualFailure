dcb3143d1c1cd5c4b60ea4c9e697b432df2787ce
Simen Lybekk <simen@lybekk.no>
2014-11-22 16:52:09 +0100

    Add a cheap excuse for a forbidden view

--- perpetualfailure/assets/__init__.py
+++ perpetualfailure/assets/__init__.py
@@ -1,3 +1,5 @@
+from pyramid.view import forbidden_view_config
+
 def configure(config):
     config.add_static_view('js', 'assets/js', cache_max_age=3600)
     config.add_static_view('fonts', 'assets/fonts', cache_max_age=3600)
@@ -5,3 +7,9 @@ def configure(config):
     config.add_route('css', '/css/{css_path:[^_][^/]*}.css')
     config.add_view(route_name='css', view='pyramid_scss.controller.get_scss', renderer='scss', request_method='GET')
 
+
+@forbidden_view_config(
+    renderer="/forbidden.mako"
+)
+def forbidden(request):
+    return {}
---
+++ perpetualfailure/templates/forbidden.mako
@@ -0,0 +1,8 @@
+<%def name="title()">Forbidden - ChatNode!</%def>
+<%inherit file="/base.mako" />
+<div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-10">
+        <h1>Forbidden</h1>
+    </div>
+</div>
